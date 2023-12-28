cbaa64f199da6d67040167f0f9ea1065d28f336a
Ruoyu Wang <wry0117@outlook.com>
2023-12-28 14:44:16 +0800

    Add scripts for px4 fw

---
+++ scripts/px4_run_dev.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+#
+# Copyright (c) 2021-2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $ROOT/utils/print_color.sh
+
+function usage() {
+    print_info "Usage: run_dev.sh" {px4_dev directory path OPTIONAL}
+}
+
+# Read and parse config file if exists
+#
+# CONFIG_IMAGE_KEY (string, can be empty)
+
+MY_ROS_DEV_DIR="$1"
+if [[ -z "$MY_ROS_DEV_DIR" ]]; then
+    MY_ROS_DEV_DIR_DEFAULTS=("$HOME/workspaces/px4-dev" "/workspaces/px4-dev")
+    for MY_ROS_DEV_DIR in "${MY_ROS_DEV_DIR_DEFAULTS[@]}"
+    do
+        if [[ -d "$MY_ROS_DEV_DIR" ]]; then
+            break
+        fi
+    done
+
+    if [[ ! -d "$MY_ROS_DEV_DIR" ]]; then
+        MY_ROS_DEV_DIR=$(realpath "$ROOT/../")
+    fi
+    print_warning "px4_dev not specified, assuming $MY_ROS_DEV_DIR"
+else
+    if [[ ! -d "$MY_ROS_DEV_DIR" ]]; then
+        print_error "Specified px4_dev does not exist: $MY_ROS_DEV_DIR"
+        exit 1
+    fi
+    shift 1
+fi
+
+ON_EXIT=()
+function cleanup {
+    for command in "${ON_EXIT[@]}"
+    do
+        $command
+    done
+}
+trap cleanup EXIT
+
+pushd . >/dev/null
+cd $ROOT
+ON_EXIT+=("popd")
+
+# Prevent running as root.
+if [[ $(id -u) -eq 0 ]]; then
+    print_error "This script cannot be executed with root privileges."
+    print_error "Please re-run without sudo and follow instructions to configure docker for non-root user if needed."
+    exit 1
+fi
+
+# Check if user can run docker without root.
+RE="\<docker\>"
+if [[ ! $(groups $USER) =~ $RE ]]; then
+    print_error "User |$USER| is not a member of the 'docker' group and cannot run docker commands without sudo."
+    print_error "Run 'sudo usermod -aG docker \$USER && newgrp docker' to add user to 'docker' group, then re-run this script."
+    print_error "See: https://docs.docker.com/engine/install/linux-postinstall/"
+    exit 1
+fi
+
+# Check if able to run docker commands.
+if [[ -z "$(docker ps)" ]] ;  then
+    print_error "Unable to run docker commands. If you have recently added |$USER| to 'docker' group, you may need to log out and log back in for it to take effect."
+    print_error "Otherwise, please check your Docker installation."
+    exit 1
+fi
+
+# Check if git-lfs is installed.
+git lfs &>/dev/null
+if [[ $? -ne 0 ]] ; then
+    print_error "git-lfs is not insalled. Please make sure git-lfs is installed before you clone the repo."
+    exit 1
+fi
+
+# Check if all LFS files are in place in the repository where this script is running from.
+cd $ROOT
+git rev-parse &>/dev/null
+if [[ $? -eq 0 ]]; then
+    LFS_FILES_STATUS=$(cd $MY_ROS_DEV_DIR && git lfs ls-files | cut -d ' ' -f2)
+    for (( i=0; i<${#LFS_FILES_STATUS}; i++ )); do
+        f="${LFS_FILES_STATUS:$i:1}"
+        if [[ "$f" == "-" ]]; then
+            print_error "LFS files are missing. Please re-clone the repo after installing git-lfs."
+            exit 1
+        fi
+    done
+fi
+
+PLATFORM="$(uname -m)"
+
+BASE_NAME="px4_dev-$PLATFORM"
+CONTAINER_NAME="$BASE_NAME-container"
+
+# Remove any exited containers.
+if [ "$(docker ps -a --quiet --filter status=exited --filter name=$CONTAINER_NAME)" ]; then
+    docker rm $CONTAINER_NAME > /dev/null
+fi
+
+# Re-use existing container.
+if [ "$(docker ps -a --quiet --filter status=running --filter name=$CONTAINER_NAME)" ]; then
+    print_info "Attaching to running container: $CONTAINER_NAME"
+    docker exec -i -t -u admin --workdir /workspaces/px4-dev $CONTAINER_NAME /bin/bash $@
+    exit 0
+fi
+
+# Build image
+BASE_IMAGE_KEY=px4_nuttx.user
+
+print_info "Building $BASE_IMAGE_KEY base as image: $BASE_NAME using key $BASE_IMAGE_KEY"
+$ROOT/build_base_image.sh $BASE_IMAGE_KEY $BASE_NAME '' '' ''
+
+if [ $? -ne 0 ]; then
+    print_error "Failed to build base image: $BASE_NAME, aborting."
+    exit 1
+fi
+
+# Map host's display socket to docker
+DOCKER_ARGS+=("-v /tmp/.X11-unix:/tmp/.X11-unix")
+DOCKER_ARGS+=("-v $HOME/.Xauthority:/home/admin/.Xauthority:rw")
+DOCKER_ARGS+=("-e DISPLAY")
+DOCKER_ARGS+=("-e NVIDIA_VISIBLE_DEVICES=all")
+DOCKER_ARGS+=("-e NVIDIA_DRIVER_CAPABILITIES=all")
+DOCKER_ARGS+=("-e FASTRTPS_DEFAULT_PROFILES_FILE=/usr/local/share/middleware_profiles/rtps_udp_profile.xml")
+DOCKER_ARGS+=("-e ROS_DOMAIN_ID")
+DOCKER_ARGS+=("-e USER")
+
+# Run container from image
+print_info "Running $CONTAINER_NAME"
+docker run -it --rm \
+    --privileged \
+    --network host \
+    ${DOCKER_ARGS[@]} \
+    -v $MY_ROS_DEV_DIR:/workspaces/px4-dev \
+    -v /dev/*:/dev/* \
+    -v /etc/localtime:/etc/localtime:ro \
+    --name "$CONTAINER_NAME" \
+    --runtime nvidia \
+    --user="admin" \
+    --entrypoint /usr/local/bin/scripts/workspace-entrypoint.sh \
+    --workdir /workspaces/px4-dev \
+    $@ \
+    $BASE_NAME \
+    /bin/bash
